28a37f36f296f0a2e4153f58efbc2aab5e6d9837
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-10-02 21:17:41 +0530

    commit day1_ preparingjs

---
+++ 7_javascript/part2/primitive.js
@@ -0,0 +1,28 @@
+// number
+let score=10 
+let rescore= new Number(100);
+
+// ()=>parenthesis
+console.log(score);
+console.log(score.valueOf());
+
+console.log(rescore);
+
+//type of : means what is typr of  data is eg no,string
+console.log(typeof(score)); //number
+console.log(typeof rescore); //object
+
+// boolean
+let isbool=true
+let isrebool=new Boolean(true) //nc
+
+console.log(isrebool);
+
+// null and undefined
+// by default data is undefined
+let first_name =undefined
+console.log(first_name);
+
+// null is ued for when we dont know the data (empty value)
+let middle_name =null
+console.log(middle_name);
