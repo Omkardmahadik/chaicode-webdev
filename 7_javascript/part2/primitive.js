3656322db6aeaa45484595630470030325281dbd
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-10-03 10:22:37 +0530

    commit primitive.js

--- 7_javascript/part2/primitive.js
+++ 7_javascript/part2/primitive.js
@@ -26,3 +26,27 @@ console.log(first_name);
 // null is ued for when we dont know the data (empty value)
 let middle_name =null
 console.log(middle_name);
+
+// string
+
+// various type of string
+let mystring = "hello"
+let mystring1 = 'hello'
+
+// for using multiple strings
+let oldgreet = mystring + " "+ "omkar";
+console.log(oldgreet);
+
+// good alternate (template literals)
+let greetmessage=`hello ${mystring} ! ${oldgreet}`;
+let demo1 = `Value is ${2+2}`
+
+console.log(greetmessage);
+console.log(demo1);
+
+// unique
+let sm1 = Symbol("om")
+let sm2 = Symbol("")
+
+console.log(sm1==sm2);
+console.log(sm1);
