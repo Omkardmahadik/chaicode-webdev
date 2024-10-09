dadb7563b3ab2fddd6506b7b25accc06263ee4c5
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-10-09 20:05:11 +0530

    commit day6_js

--- 7_javascript/part3/02_loopschallenges.js
+++ 7_javascript/part3/02_loopschallenges.js
@@ -3,11 +3,44 @@
 numbers from 1 to 5 and stores the result in a variable
 named 'sum`.
 */
-let sum=0;
-let i=1;
-while (i<=5) {
-    sum = sum+i
-    i++
+let sum = 0;
+let i = 1;
+while (i <= 10) {
+    sum = sum + i
+    i++//increment
 }
-console.log(sum );
-console.log(sum );
+console.log(sum);
+
+/*
+2. Write a while loop that counts down from 5 to 1 and
+stores the numbers in an array named 'countdown'`.
+
+
+*/
+
+let countdown=[];
+let j=5;
+
+while (j > 0) {
+    countdown.push(j)
+    j--;
+}
+console.log(countdown);
+
+
+/*
+3. Write a 'do while loop that prompts a user to enter
+their favorite tea type until they enter ""stop"`.
+Store each tea type in an array named `teaCollection'.
+*/
+let teacollection=[]
+let user
+do{
+    user=prompt(`Enter your tea type: (type "stop" to finish)`);
+    if(user!=="stop")
+    {
+       teacollection.push(user)
+    }
+}
+while(user!=="stop")
+
