adbe87a5c18a0b3cf6d8c083e885c554b879e013
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-10-10 22:03:13 +0530

    commit day10_js

--- 7_javascript/part3/02_loopschallenges.js
+++ 7_javascript/part3/02_loopschallenges.js
@@ -1,46 +1,143 @@
-/*
-1. Write a while loop that calculates the sum of all
-numbers from 1 to 5 and stores the result in a variable
-named 'sum`.
+/* 1: Declare an array named teaFlavors
+the strings "green_tea"`, "black
+tea"`.
+Access the first element of the
+in a variable named `firstTea`·*/
+
+/* notes
+  [0]-> ise indexing bolte he jis ham array ko access kar sakhte he
 */
-let sum = 0;
-let i = 1;
-while (i <= 10) {
-    sum = sum + i
-    i++//increment
-}
-console.log(sum);
+let teaFlavors = [
+    "green_tea",
+    "blacktea"
+]
 
-/*
-2. Write a while loop that counts down from 5 to 1 and
-stores the numbers in an array named 'countdown'`.
+// 2nd way to use array
+// let teaFlavorss= new Array[
+//     "green_tea",
+//     "blacktea"
+// ]
 
+let firstTea = teaFlavors[0]
+console.log(firstTea);
 
+/* 2: Declare an array named 'cities' containing
+"London", "Tokyo", "Paris", and "New York".
+Access the third element in the array and store it
+in a variable named 'favoriteCity`.*/
+
+let cities = ["London", "Tokyo", "Paris", "New York"]
+
+let favoriteCity = cities[3]
+console.log(favoriteCity);
+
+/* 3: You have an array named 'teaTypes` containing
+"herbal tea", "white tea", and "masala chai"`.
+Change the second element of the array to "jasmine
+tea".
 */
 
-let countdown=[];
-let j=5;
+let teaTypes = ["herbal tea", "white tea", "masala chai"]
 
-while (j > 0) {
-    countdown.push(j)
-    j--;
-}
-console.log(countdown);
+teaTypes[2] = "jasminetea"
 
+console.log(teaTypes);
+
+/* 4: Declare an array named cities Visited containing
+"Mumbai" and "Sydney",
+Add "Berlin" to the array using the `push` method.*/
+
+let citiess = ["Mumbai", "sydney"]
+
+cities[2] = "Berlin" //..
+cities.push("Berlin")
+
+console.log(citiess);
+
+/* 5: You have an array named 'teaOrders' with '"chai"`,
+""iced tea", "matcha", and "earl grey"`.
+Remove the last element of the array using the 'pop'
+method and store it in a variable named 'lastOrder`.*/
+
+
+let teaOrders = ["chai", "icetea", "matcha", "earl grey"]
+
+// remove the last item of the array
+teaOrders.pop()
+lastorder = teaOrders
+console.log(lastorder);
+
+/* 6: You have an array named `popularTeas` containing
+"green tea", "oolong tea", and "chai"`.
+Create a soft copy of this array named
+`softCopyTeas=change in both array`. >
+*/
+
+let popularTeas = ["green tea", "oolang tea", "chai"]
+
+popularTeas
+
+console.log(popularTeas);
 
 /*
-3. Write a 'do while loop that prompts a user to enter
-their favorite tea type until they enter ""stop"`.
-Store each tea type in an array named `teaCollection'.
+7: You have an array named 'topCities' containing
+"Berlin", "Singapore", and "New York".
+Create a hard copy of this array named
+`hardCopyCities =change in single array`.
 */
-let teacollection=[]
-let user
-do{
-    user=prompt(`Enter your tea type: (type "stop" to finish)`);
-    if(user!=="stop")
-    {
-       teacollection.push(user)
-    }
-}
-while(user!=="stop")
+ 
+let topCities =["Berlin","Singapore","New York"]
+let hardCopyCities = [...topCities]
+// let hardCopyCities = topCities.slice() //works same
+// topCities.pop()
 
+// console.log(topCities);
+
+console.log(hardCopyCities);
+
+// rest and spread operator(later)
+// console.log([...cities.hardCopyCities]);
+
+
+/**
+ 8. You have two arrays: europeanCities' containing
+"Paris" and "Rome", and asianCities' containing
+"Tokyo" and "Bangkok"`.
+Merge these two arrays into a new array named
+'worldCities'.
+*/
+
+let europeanCities=["paris","Rome"]
+let asianCities=["Tokyo","Bangkok"]
+
+// worldcities = europeanCities.concat(asianCities)
+worldcities = [...asianCities,...europeanCities]
+console.log(worldcities);
+
+/*
+ 9. You have an array named 'teaMenu' containing
+"masala chai", "oolong tea", "green tea", and
+"earl grey",
+Find the length of the array and store it in a
+variable named `menuLength`.
+*/
+
+let teamenu = ["masala chai","oolong tea","green tea","earl grey"]
+
+menulength=teamenu.length
+
+console.log(menulength);
+
+/*
+10. You have an array named `cityBucketList' containing
+"Kyoto", "London", "Cape Town", and
+"Vancouver",
+Check if "London" is in the array and store the >
+result in a variable named 'isLondonInList`.
+*/
+let cityBucketList=["Kyoto", "London", "Cape Town","Vancouver"]
+
+// let isLondonInList=cityBucketList[1]
+let isLondonInList=cityBucketList.includes("London")
+
+console.log("isLondonInList:",isLondonInList);
