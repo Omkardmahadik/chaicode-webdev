0b329cc4389ff25a3c19af4c1ae1788fee1e90b8
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-10-12 20:04:17 +0530

    commit part3

--- 7_javascript/part3/02_loopschallenges.js
+++ 7_javascript/part3/02_loopschallenges.js
@@ -142,16 +142,3 @@ let isLondonInList=cityBucketList.includes("London")
 
 console.log("isLondonInList:",isLondonInList);
 
-/*
-10. You have an array named `cityBucketList' containing
-"Kyoto", "London", "Cape Town", and
-"Vancouver",
-Check if "London" is in the array and store the >
-result in a variable named 'isLondonInList`.
-*/
-// let cityBucketList=["Kyoto", "London", "Cape Town","Vancouver"]
-
-// // let isLondonInList=cityBucketList[1]
-// let isLondonInList=cityBucketList.includes("London")
-
-// console.log("isLondonInList:",isLondonInList);
