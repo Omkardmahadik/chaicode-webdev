3bd75573322c7fba7e9b5b8cc05c3cc8062fef28
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-10-12 20:05:07 +0530

    commit 03_js

---
+++ 7_javascript/part3/03_levelupchallenges.js
@@ -0,0 +1,18 @@
+/*
+1. Write a 'for' loop that loops through the array
+["green tea", "black tea", "chai", "oolong tea" ]' and
+stops the loop when it finds ""chai"'.
+Store all teas before ""chai"' in a new array named
+selectedTeas'
+*/
+
+let tea=["green tea", "black tea", "chai", "oolong tea"]
+let selectedTeas=[]
+for(let i=0 ; i<tea.length;i++)
+{
+    if(tea[i]=='chai'){
+       break;
+    }
+   selectedTeas.push(tea[i])
+}
+console.log(selectedTeas);
