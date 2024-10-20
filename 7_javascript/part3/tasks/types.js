fabeca84c558355930d0ea299d5e25528c8eb3fa
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-10-20 20:13:55 +0530

    commit tasks

---
+++ 7_javascript/part3/tasks/types.js
@@ -0,0 +1,37 @@
+
+// Write a function stringToNumber that takes a string input and tries to convert it to a number. If the conversion fails, return "Not a number".
+
+// function stringToNumber(input) {
+//   let num = Number(num)
+//   if(typeof(num))
+//   {
+//     console.log("its a number");
+//   }
+//   return num
+// }
+
+// let result = stringToNumber()
+
+// if(typeof result =="number")
+// {
+//     console.log("number");
+    
+// }
+// else{
+//     console.log("not a number");  
+// }
+
+
+// Write a function flipBoolean that takes any input and converts it to its boolean equivalent, then flips it. For example, true becomes false, 0 becomes true, etc.
+
+function flipBoolean(input){
+   let i = Boolean(input)
+
+   if(isNaN(i))
+   {
+      console.log("value is no");
+   }
+}
+
+
+
