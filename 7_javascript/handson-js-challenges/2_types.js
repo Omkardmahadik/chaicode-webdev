6eb0962233c7fa5d06db19261495fced8154a1e2
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-10-22 20:52:07 +0530

    commit js

--- 7_javascript/handson-js-challenges/2_types.js
+++ 7_javascript/handson-js-challenges/2_types.js
@@ -20,3 +20,28 @@ function flipBoolean(input)
 }
 n=flipBoolean(true);
 console.log(n);
+
+// Task 3:
+// Write a function whatAmI that takes an input and returns a string describing its type after conversion. If it's a number, return "I'm a number!", if it's a string, return "I'm a string!"
+
+function whatAmI(input) {
+   // Convert input to number
+   const num = Number(input);
+ 
+   // Check if the input is a number and is not NaN
+   if (!isNaN(num) && typeof input !== 'string') {
+     return "I'm a number!";
+   }
+   // Check if the input is a string
+   else if (typeof input === 'string') {
+     return "I'm a string!";
+   } else {
+     return "I'm something else!";
+   }
+ }
+ 
+ // Example usage:
+ console.log(whatAmI(2));        // Output: I'm a number!
+ console.log(whatAmI("hello"));  // Output: I'm a string!
+ console.log(whatAmI(null));     // Output: I'm something else!
+ console.log(whatAmI(undefined)); // Output: I'm something else!
