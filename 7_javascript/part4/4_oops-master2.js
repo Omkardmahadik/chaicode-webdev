71e37a6d14f9ce07f535204aa9ac6689170f1d4b
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-11-01 18:43:28 +0530

    commit oops-part2

--- 7_javascript/part4/4_oops-master2.js
+++ 7_javascript/part4/4_oops-master2.js
@@ -47,3 +47,39 @@ console.log(mymachine.brewcoffee());
 console.log(mymachine.pressbutton());
 
 // polymorpism
+/**
+ Polymorphism ka matlab hota hai "one name, multiple forms." Yeh ek concept hai object-oriented programming mein, jisme ek function ya method different classes mein different tareeke se behave karta hai, par uska naam same hota hai.
+ */
+class Bird{
+    fly(){
+        return `flying...`;
+    }
+}
+
+class penguin extends Bird{
+    fly(){
+        return `penguin can fly`;
+    }
+}
+
+let bird = new Bird()
+let Penguin = new penguin()
+console.log(bird.fly());
+console.log(Penguin.fly());
+
+// static method
+// no method can access static method
+// only class can access the static method
+class Calculator{
+    static add(a,b){
+        return a+b;
+    }
+}
+/* you cannot access using another variable static method only by accessing using class only -> try this
+*/
+/*let calcs = new Calculator()
+console.log(calcs.add(2,3));*/
+
+
+let calc = new Calculator()
+console.log(Calculator.add(2,3));
