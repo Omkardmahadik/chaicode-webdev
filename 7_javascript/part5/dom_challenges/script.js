18dd11d7bdf8622306e938e93a3cc5cdbe282e2c
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-11-04 12:54:41 +0530

    commit dom-challenges-1

---
+++ 7_javascript/part5/dom_challenges/script.js
@@ -0,0 +1,17 @@
+// popup in the browser
+// alert("welcome to the website:)")
+
+// example-1
+
+// higher order function
+document.getElementById(`changeTextButton`).addEventListener(`click`, function () {
+    let Paragraph = document.getElementById(`myParagraph`);
+    //   console.log(Paragraph); //show the full element
+    //   console.log(Paragraph.textContent) //show the paragraph when i click on button
+
+    Paragraph.textContent = "website is in maintaianance";
+});
+
+
+// example-2
+
