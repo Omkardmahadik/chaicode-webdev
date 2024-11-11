c475dcb1c7414693332ff304ddf1e63aec7371f1
Omkar Mahadik <Omkardmahadik@gmail.com>
2024-11-11 10:38:33 +0530

    commit dom-finale-halfpart-done

--- 7_javascript/part5/dom_finale/script.js
+++ 7_javascript/part5/dom_finale/script.js
@@ -1,13 +1,25 @@
-// Example 6: eventListner types
+// Example 6: 
 document.getElementById(`clickMeButton`).addEventListener(`dblclick`,function(){
     alert("helloWorld");
 })
 
-// Example 7: 
+// Example 7
 document.getElementById(`teaList`).addEventListener(`click`,function(event){
     if(event.target && event.target.matches(`.teaItem`)){
         alert("you Selected:" + event.target.textContent);
         
     }
     
+})
+
+// Example 8:
+document.getElementById(`feedbackForm`).addEventListener(`submit` , function(event){
+   event.preventDefault();
+
+   let feedback = document.getElementById(`feedbackInput`).value;
+   console.log(feedback);
+   document.getElementById(`feedbackDisplay`).textContent= `Feedback is: ${feedback}`
+
+//    hw: how to grab the value of feedbackinput
+   
 })
